42ab3a6a2904131572e963c6311f47f2f8e15c78
Valeria Matieva <foystina@gmail.com>
2020-03-23 19:25:34 +0400

    feat: console output

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var round = ['()'];
+  var square = ['[]'];
+  var curved = ['{}'];
+  var straight = ['||'];
+  for(let i = 0; i <= str.length; i++){
+    if(str.includes(round)) {
+     str = str.replace(round, "");
+    }
+    else if(str.includes(square)) {
+      str = str.replace(square, "");
+    }
+    else if(str.includes(curved)) {
+      str = str.replace(curved, "");
+    }
+    else if(str.includes(straight)) {
+      str = str.replace(straight, "");
+    }
+  }
+  return str.length ? false : true;
 }
